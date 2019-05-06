fc05dccc9b6517b388899a86533a824fead555de
JustinMShea <jshea@roosevelt.edu>
2019-05-06 00:02:25 -0500

    REorg of script, added comparisons.

--- benchmarks.R
+++ benchmarks.R
@@ -1,39 +1,48 @@
 library(benchmarkme)
 
-BLIS <- benchmark_std()
-plot(BLIS, blas_optimize = TRUE)
-saveRDS(BLIS, "data/BLIS.rds")
-rm(BLIS)
+BLISZEN <- benchmark_std()
+saveRDS(BLISZEN, "data/BLISZEN.rds")
+rm(BLISZEN)
 gc()
 
-BLIS_1 <- benchmark_std(runs = 6, cores = 1)
-plot(BLIS_1, blas_optimize = NULL)
-saveRDS(BLIS_1, "data/BLIS_1.rds")
-rm(BLIS_1)
+BLISZEN <- benchmark_std(runs = 6, cores = 1)
+saveRDS(BLISZEN_1, "data/BLISZEN_1.rds")
+rm(BLISZEN_1)
 gc()
 
-BLIS_4 <- benchmark_std(cores = 4)
-plot(BLIS_4, blas_optimize = NULL)
-saveRDS(BLIS_4, "data/BLIS_4.rds")
-rm(BLIS_4)
+BLISZEN_4 <- benchmark_std(cores = 4)
+saveRDS(BLISZEN_4, "data/BLISZEN_4.rds")
+rm(BLISZEN_4)
 gc()
 
-BLIS_8 <- benchmark_std(cores = 8)
-plot(BLIS_8, blas_optimize = NULL)
-saveRDS(BLIS_8, "data/BLIS_8.rds")
-rm(BLIS_8)
+BLISZEN_8 <- benchmark_std(cores = 8)
+saveRDS(BLISZEN_8, "data/BLISZEN_8.rds")
+rm(BLISZEN_8)
 gc()
 
-BLIS_16 <- benchmark_std(cores = 16)
-plot(BLIS_16, blas_optimize = NULL)
-saveRDS(BLIS_16, "data/BLIS_16.rds")
-rm(BLIS_16)
+BLISZEN_16 <- benchmark_std(cores = 16)
+saveRDS(BLISZEN_16, "data/BLISZEN_16.rds")
+rm(BLISZEN_16)
 gc()
 
-BLIS_32 <- benchmark_std(cores = 32)
-plot(BLIS_32, blas_optimize = NULL)
-saveRDS(BLIS_32, "data/BLIS_32.rds")
-rm(BLIS_32)
+BLISZEN_32 <- benchmark_std(cores = 32)
+saveRDS(BLISZEN_32, "data/BLISZEN_32.rds")
+rm(BLISZEN_32)
 gc()
 
+plot(BLISZEN, blas_optimize = NULL)
+plot(BLISZEN_1, blas_optimize = NULL)
+plot(BLISZEN_4, blas_optimize = NULL)
+plot(BLISZEN_8, blas_optimize = NULL)
+plot(BLISZEN_16, blas_optimize = NULL)
+plot(BLISZEN_32, blas_optimize = NULL)
 
+sum(BLIS_8[BLIS_8$test_group=="matrix_fun","elapsed"])/3
+sum(BLISZEN_8[BLISZEN_8$test_group=="matrix_fun","elapsed"])/3
+sum(BLIS_32[BLIS_32$test_group=="matrix_fun","elapsed"])/3
+
+sum(BLIS_16[BLIS_16$test_group=="matrix_fun","elapsed"])/3
+sum(BLISZEN_16[BLISZEN_16$test_group=="matrix_fun","elapsed"])/3
+
+sum(BLIS_32[BLIS_32$test_group=="matrix_fun","elapsed"])/3
+sum(BLISZEN_32[BLISZEN_32$test_group=="matrix_fun","elapsed"])/3
