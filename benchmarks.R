d7ceb422739738f1777706df9c35ebf820254fcd
JustinMShea <jshea@roosevelt.edu>
2019-09-08 21:49:48 -0500

    recent updates, names

--- benchmarks.R
+++ benchmarks.R
@@ -1,48 +1,57 @@
 library(benchmarkme)
 
-BLISZEN <- benchmark_std()
-saveRDS(BLISZEN, "data/BLISZEN.rds")
-rm(BLISZEN)
+AMD_BLIS_libFLAME <- benchmark_std()
+saveRDS(AMD_BLIS_libFLAME, "data/AMD_BLIS_libFLAME.rds")
+rm(AMD_BLIS_libFLAME)
 gc()
 
-BLISZEN_1 <- benchmark_std(runs = 6, cores = 1)
-saveRDS(BLISZEN_1, "data/BLISZEN_1.rds")
-rm(BLISZEN_1)
+
+AMD_BLIS_libFLAME_1 <- benchmark_std(runs = 6, cores = 1)
+saveRDS(AMD_BLIS_libFLAME_1, "data/AMD_BLIS_libFLAME_1.rds")
+rm(AMD_BLIS_libFLAME_1)
 gc()
 
-BLISZEN_4 <- benchmark_std(cores = 4)
-saveRDS(BLISZEN_4, "data/BLISZEN_4.rds")
-rm(BLISZEN_4)
+AMD_BLIS_libFLAME_2 <- benchmark_std()
+saveRDS(AMD_BLIS_libFLAME_2, "data/AMD_BLIS_libFLAME_2.rds")
+rm(AMD_BLIS_libFLAME_2)
 gc()
 
-BLISZEN_8 <- benchmark_std(cores = 8)
-saveRDS(BLISZEN_8, "data/BLISZEN_8.rds")
-rm(BLISZEN_8)
+
+AMD_BLIS_libFLAME_4 <- benchmark_std(cores = 4)
+saveRDS(AMD_BLIS_libFLAME_4, "data/AMD_BLIS_libFLAME_4.rds")
+rm(AMD_BLIS_libFLAME_4)
 gc()
 
-BLISZEN_16 <- benchmark_std(cores = 16)
-saveRDS(BLISZEN_16, "data/BLISZEN_16.rds")
-rm(BLISZEN_16)
+AMD_BLIS_libFLAME_8 <- benchmark_std(cores = 8)
+saveRDS(AMD_BLIS_libFLAME_8, "data/AMD_BLIS_libFLAME_8.rds")
+rm(AMD_BLIS_libFLAME_8)
 gc()
 
-BLISZEN_32 <- benchmark_std(cores = 32)
-saveRDS(BLISZEN_32, "data/BLISZEN_32.rds")
-rm(BLISZEN_32)
+
+AMD_BLIS_libFLAME_16 <- benchmark_std(cores = 16)
+saveRDS(AMD_BLIS_libFLAME_16, "data/AMD_BLIS_libFLAME_16.rds")
+rm(AMD_BLIS_libFLAME_16)
 gc()
 
-plot(BLISZEN, blas_optimize = NULL)
-plot(BLISZEN_1, blas_optimize = NULL)
-plot(BLISZEN_4, blas_optimize = NULL)
-plot(BLISZEN_8, blas_optimize = NULL)
-plot(BLISZEN_16, blas_optimize = NULL)
-plot(BLISZEN_32, blas_optimize = NULL)
+AMD_BLIS_libFLAME_32 <- benchmark_std(cores = 32)
+saveRDS(AMD_BLIS_libFLAME_32, "data/AMD_BLIS_libFLAME_32.rds")
+rm(AMD_BLIS_libFLAME_32)
+gc()
+
+plot(AMD_BLIS_libFLAME, blas_optimize = NULL)
+plot(AMD_BLIS_libFLAME_1, blas_optimize = NULL)
+plot(AMD_BLIS_libFLAME_2, blas_optimize = NULL)
+plot(AMD_BLIS_libFLAME_4, blas_optimize = NULL)
+plot(AMD_BLIS_libFLAME_8, blas_optimize = NULL)
+plot(AMD_BLIS_libFLAME_16, blas_optimize = NULL)
+plot(AMD_BLIS_libFLAME_32, blas_optimize = NULL)
 
 sum(BLIS_8[BLIS_8$test_group=="matrix_fun","elapsed"])/3
-sum(BLISZEN_8[BLISZEN_8$test_group=="matrix_fun","elapsed"])/3
+sum(AMD_BLIS_libFLAME_8[AMD_BLIS_libFLAME_8$test_group=="matrix_fun","elapsed"])/3
 sum(BLIS_32[BLIS_32$test_group=="matrix_fun","elapsed"])/3
 
 sum(BLIS_16[BLIS_16$test_group=="matrix_fun","elapsed"])/3
-sum(BLISZEN_16[BLISZEN_16$test_group=="matrix_fun","elapsed"])/3
+sum(AMD_BLIS_libFLAME_16[AMD_BLIS_libFLAME_16$test_group=="matrix_fun","elapsed"])/3
 
 sum(BLIS_32[BLIS_32$test_group=="matrix_fun","elapsed"])/3
-sum(BLISZEN_32[BLISZEN_32$test_group=="matrix_fun","elapsed"])/3
+sum(AMD_BLIS_libFLAME_32[AMD_BLIS_libFLAME_32$test_group=="matrix_fun","elapsed"])/3
