693b0ca645052ca092f4799c8607193b2aaf9827
JustinMShea <jshea@roosevelt.edu>
2020-09-23 22:14:16 -0500

    openBLAS

---
+++ openBLAS-benchmarks.R
@@ -0,0 +1,42 @@
+library(benchmarkme)
+
+get_linear_algebra()
+
+
+openBLAS_openmp_openmp <- benchmark_std()
+attr(openBLAS_openmp, "BLAS") <- get_linear_algebra()
+attr(openBLAS_openmp, "SystemTime") <- Sys.time()
+saveRDS(openBLAS_openmp, "data/2950x/openBLAS_openmp.rds")
+rm(openBLAS_openmp)
+gc()
+
+
+openBLAS_openmp_1 <- benchmark_std(cores = 1)
+attr(openBLAS_openmp_1, "BLAS") <- get_linear_algebra()
+attr(openBLAS_openmp_1, "SystemTime") <- Sys.time()
+saveRDS(openBLAS_openmp_1, "data/2950x/openBLAS_openmp_1.rds")
+rm(openBLAS_openmp_1)
+gc()
+
+
+openBLAS_openmp_4 <- benchmark_std(cores = 4)
+attr(openBLAS_openmp_4, "BLAS") <- get_linear_algebra()
+attr(openBLAS_openmp_4, "SystemTime") <- Sys.time()
+saveRDS(openBLAS_openmp_4, "data/2950x/openBLAS_openmp_4.rds")
+rm(openBLAS_openmp_4)
+gc()
+
+openBLAS_openmp_8 <- benchmark_std(cores = 8)
+attr(openBLAS_openmp_8, "BLAS") <- get_linear_algebra()
+attr(openBLAS_openmp_8, "SystemTime") <- Sys.time()
+saveRDS(openBLAS_openmp_8, "data/2950x/openBLAS_openmp_8.rds")
+rm(openBLAS_openmp_8)
+gc()
+
+
+openBLAS_openmp_16 <- benchmark_std(cores = 16)
+attr(openBLAS_openmp_16, "BLAS") <- get_linear_algebra()
+attr(openBLAS_openmp_16, "SystemTime") <- Sys.time()
+saveRDS(openBLAS_openmp_16, "data/2950x/openBLAS_openmp_16.rds")
+rm(openBLAS_openmp_16)
+gc()
