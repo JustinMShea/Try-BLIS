18ead89dff1659112e6ddea0b0e9aa35d15c7a1c
JustinMShea <jshea@roosevelt.edu>
2020-09-23 20:19:04 -0500

    bliz zenlibFLAME benchmarks

---
+++ BLIS-zenlibFLAME-benchmarks.R
@@ -0,0 +1,42 @@
+library(benchmarkme)
+
+get_linear_algebra()
+
+
+BLIS_zenlibFLAME <- benchmark_std()
+attr(BLIS_zenlibFLAME, "BLAS") <- get_linear_algebra()
+attr(BLIS_zenlibFLAME, "SystemTime") <- Sys.time()
+saveRDS(BLIS_zenlibFLAME, "data/2950x/BLIS_zenlibFLAME.rds")
+rm(BLIS_zenlibFLAME)
+gc()
+
+
+BLIS_zenlibFLAME_1 <- benchmark_std(runs = 6, cores = 1)
+attr(BLIS_zenlibFLAME_1, "BLAS") <- get_linear_algebra()
+attr(BLIS_zenlibFLAME_1, "SystemTime") <- Sys.time()
+saveRDS(BLIS_zenlibFLAME_1, "data/2950x/BLIS_zenlibFLAME_1.rds")
+rm(BLIS_zenlibFLAME_1)
+gc()
+
+
+BLIS_zenlibFLAME_4 <- benchmark_std(cores = 4)
+attr(BLIS_zenlibFLAME_4, "BLAS") <- get_linear_algebra()
+attr(BLIS_zenlibFLAME_4, "SystemTime") <- Sys.time()
+saveRDS(BLIS_zenlibFLAME_4, "data/2950x/BLIS_zenlibFLAME_4.rds")
+rm(BLIS_zenlibFLAME_4)
+gc()
+
+BLIS_zenlibFLAME_8 <- benchmark_std(cores = 8)
+attr(BLIS_zenlibFLAME_8, "BLAS") <- get_linear_algebra()
+attr(BLIS_zenlibFLAME_8, "SystemTime") <- Sys.time()
+saveRDS(BLIS_zenlibFLAME_8, "data/2950x/BLIS_zenlibFLAME_8.rds")
+rm(BLIS_zenlibFLAME_8)
+gc()
+
+
+BLIS_zenlibFLAME_16 <- benchmark_std(cores = 16)
+attr(BLIS_zenlibFLAME_16, "BLAS") <- get_linear_algebra()
+attr(BLIS_zenlibFLAME_16, "SystemTime") <- Sys.time()
+saveRDS(BLIS_zenlibFLAME_16, "data/2950x/BLIS_zenlibFLAME_16.rds")
+rm(BLIS_zenlibFLAME_16)
+gc()
