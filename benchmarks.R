9a6ca4f87b5bf422f884272e133c83a8f8f9e08a
JustinMShea <jshea@roosevelt.edu>
2020-09-23 20:41:44 -0500

    modified og script, seperate out EDA

--- benchmarks.R
+++ benchmarks.R
@@ -3,7 +3,9 @@ library(benchmarkme)
 get_linear_algebra()
 
 AMD_BLIS_libFLAME <- benchmark_std()
-saveRDS(AMD_BLIS_libFLAME, "data/AMD_BLIS_libFLAME.rds")
+attr(AMD_BLIS_libFLAME, "BLAS") <- get_linear_algebra()
+
+saveRDS(AMD_BLIS_libFLAME, "data/2950x/AMD_BLIS_libFLAME.rds")
 rm(AMD_BLIS_libFLAME)
 gc()
 
@@ -40,20 +42,4 @@ saveRDS(AMD_BLIS_libFLAME_32, "data/AMD_BLIS_libFLAME_32.rds")
 rm(AMD_BLIS_libFLAME_32)
 gc()
 
-plot(AMD_BLIS_libFLAME, blas_optimize = NULL)
-plot(AMD_BLIS_libFLAME_1, blas_optimize = NULL)
-plot(AMD_BLIS_libFLAME_2, blas_optimize = NULL)
-plot(AMD_BLIS_libFLAME_4, blas_optimize = NULL)
-plot(AMD_BLIS_libFLAME_8, blas_optimize = NULL)
-plot(AMD_BLIS_libFLAME_16, blas_optimize = NULL)
-plot(AMD_BLIS_libFLAME_32, blas_optimize = NULL)
 
-sum(BLIS_8[BLIS_8$test_group=="matrix_fun","elapsed"])/3
-sum(AMD_BLIS_libFLAME_8[AMD_BLIS_libFLAME_8$test_group=="matrix_fun","elapsed"])/3
-sum(BLIS_32[BLIS_32$test_group=="matrix_fun","elapsed"])/3
-
-sum(BLIS_16[BLIS_16$test_group=="matrix_fun","elapsed"])/3
-sum(AMD_BLIS_libFLAME_16[AMD_BLIS_libFLAME_16$test_group=="matrix_fun","elapsed"])/3
-
-sum(BLIS_32[BLIS_32$test_group=="matrix_fun","elapsed"])/3
-sum(AMD_BLIS_libFLAME_32[AMD_BLIS_libFLAME_32$test_group=="matrix_fun","elapsed"])/3
